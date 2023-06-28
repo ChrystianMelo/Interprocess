931c09c1711dc7e54b35de9b4cd9b2d37e454c30
Chrystian Melo <chrysmelao@gmail.com>
2023-06-28 15:55:01 -0300

    Refactor resource management: Utilize boost::interprocess for efficient resource utilization

--- Interprocess/Main.cpp
+++ Interprocess/Main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
 #include <string>
 #include <string_view>
 #include <chrono>
 #include <thread>
 #include <filesystem>
+#include <mutex>
+#include <cstdio>
 
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
@@ -14,133 +15,223 @@
 #include <boost/interprocess/exceptions.hpp>
 #include <boost/interprocess/sync/interprocess_condition.hpp>
 
-#include <mutex>
+/**
+ * @brief Nome da memória compartilhada utilizada para comunicação entre instâncias.
+ */
+const std::string_view shared_memory_name = "InstanceManagement11";
 
-const std::string_view shared_memory_name = "InstanceManagmment";
-const std::string_view mutex_name = "InstanceMutex";
+/**
+ * @brief Nome do mutex utilizado para controlar o acesso à memória compartilhada.
+ */
+const std::string_view mutex_name = "InstanceMutex11";
 
-void doInstanceCommunication(std::function<void()> running, std::function<void()> notRunning) {
-	// Crie ou abra o objeto de memória compartilhada
+/**
+ * @brief Tamanho máximo da mensagem a ser armazenada na memória compartilhada.
+ */
+const int messageSize = 100;
+
+/**
+ * @brief Classe SharedData, representando os dados compartilhados entre os processos. Ela contém as estruturas e variáveis necessárias para
+ * a comunicação e sincronização entre os processos por meio de memória compartilhada.
+ */
+class SharedData
+{
+public:
+	/**
+	 * @brief Construtor da classe SharedData.
+	 */
+	SharedData()
+		: message_in(false)
+	{}
+
+	/**
+	 * @brief Mutex para proteger o acesso aos recursos compartilhados.
+	 */
+	boost::interprocess::interprocess_mutex mutex;
+
+	/**
+	 * @brief Condição para aguardar quando a fila estiver vazia.
+	 */
+	boost::interprocess::interprocess_condition condition;
+
+	/**
+	 * @brief Array para armazenar os itens a serem preenchidos.
+	 */
+	char items[messageSize];
+
+	/**
+	 * @brief Flag indicando se há alguma mensagem presente.
+	 */
+	bool message_in;
+};
+
+/**
+ * @brief Realiza a comunicação entre as instâncias/processos. Recebe duas funções como parâmetro: mainInstance e secundaryInstance, que serão
+ * executadas dependendo se a instância atual estiver executando ou não. A função verifica se outra instância da aplicação
+ * já está em execução e tenta bloquear o mutex para controlar o acesso aos recursos compartilhados.
+ *
+ * @param mainInstance Função a ser executada como instância principal.
+ * @param secundaryInstance Função a ser executada como instancia secundária.
+ */
+void coordinateInstanceCommunication(std::function<void()> mainInstance, std::function<void()> secundaryInstance) {
+	// Cria ou abre o objeto de memória compartilhada
 	boost::interprocess::shared_memory_object shared_memory(boost::interprocess::open_or_create, shared_memory_name.data(), boost::interprocess::read_write);
 
-	// Defina o tamanho da região de memória compartilhada
+	// Define o tamanho da região de memória compartilhada
 	shared_memory.truncate(sizeof(bool));
 
-	// Mapeie a região de memória compartilhada
+	// Mapeia a região de memória compartilhada
 	boost::interprocess::mapped_region region(shared_memory, boost::interprocess::read_write);
 
-	// Verifique se outra instância da aplicação está em execução e tente bloquear o mutex
-	bool* is_running = static_cast<bool*>(region.get_address());
+	// Verifica se outra instância da aplicação está em execução e tenta bloquear o mutex
+	bool* is_mainInstance = static_cast<bool*>(region.get_address());
 	boost::interprocess::named_mutex mutex(boost::interprocess::open_or_create, mutex_name.data());
 
-	if (*is_running || !mutex.try_lock())
+	if (*is_mainInstance || !mutex.try_lock())
 	{
 		std::cout << "Outra instância da aplicação já está em execução." << std::endl;
 
-		notRunning();
+		secundaryInstance();
 
-		*is_running = true;
-
+		*is_mainInstance = true;
 	}
 	else {
 		// O bloqueio do mutex foi bem-sucedido, execute a lógica da sua aplicação aqui
 		std::cout << "Aplicação está sendo executada" << std::endl;
 
-		running();
+		mainInstance();
 
 		// Após concluir o processamento, desbloqueie o mutex e marque o objeto de memória compartilhada
 		mutex.unlock();
-		*is_running = false;
-	}
-}
-
-std::string read() {
-	std::string msg;
-	try
-	{
-		boost::interprocess::managed_shared_memory shared_memory(boost::interprocess::open_only, shared_memory_name.data());
-
-		boost::interprocess::interprocess_semaphore* semaphore = shared_memory.find<boost::interprocess::interprocess_semaphore>("InstanceSemaphore").first;
-
-		std::string* shared_message = shared_memory.find<std::string>("InstanceMessage").first;
-
-		semaphore->wait();
-
-		std::cout << "Mensagem recebida: " << *shared_message << std::endl;
-
-		return *shared_message;
-	}
-	catch (const std::exception& ex)
-	{
-		boost::interprocess::shared_memory_object::remove(shared_memory_name.data());
+		*is_mainInstance = false;
 	}
 
-	return msg;
-
-}
-
-bool write(const std::string_view msg) {
-
-	try
-	{
-		boost::interprocess::managed_shared_memory shared_memory(boost::interprocess::open_or_create, shared_memory_name.data(), 65536);
-
-		boost::interprocess::interprocess_semaphore* semaphore = shared_memory.find_or_construct<boost::interprocess::interprocess_semaphore>("InstanceSemaphore")(0);
-
-		std::string* shared_message = shared_memory.find_or_construct<std::string>("InstanceMessage")("");
-
-		*shared_message = msg.data();
-
-		semaphore->post();
-
-		return true;
-	}
-	catch (const std::exception& ex)
-	{
-		boost::interprocess::shared_memory_object::remove(shared_memory_name.data());
-		return false;
-	}
+	boost::interprocess::shared_memory_object::remove(shared_memory_name.data());
 }
 
 int main()
 {
-	std::function<void()> reader = [&]() {
-		std::cout << "Aberto para conexões..." << std::endl;
-		bool systemAvailable = true;
+	bool isMainInstanceExecuting = true;
+	const char* shared_memory_name = "InstanceCommunication";
 
-		std::string msg;
-		for (; msg.empty(); msg = read()) std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+	// Gerencia a comunicação entre as instancias
+	coordinateInstanceCommunication([&]() {
+		while (isMainInstanceExecuting) {
+			// Apaga a memória compartilhada anterior e agenda a remoção na saída
+			boost::interprocess::shared_memory_object::remove(shared_memory_name);
+			
+			// Cria um objeto de memória compartilhada
+			boost::interprocess::shared_memory_object object(
+				boost::interprocess::create_only,  // somente criação
+				shared_memory_name,                  // nome
+				boost::interprocess::read_write     // modo leitura-escrita
+			);
 
-		std::string result;
-		if (msg.compare("readModel") == 0 && systemAvailable)
-			result = "Lendo modelo";
-		else
-			result = "Comando negado";
+			try {
+				// Define o tamanho
+				object.truncate(sizeof(SharedData));
 
-		std::cout << result << std::endl;
+				// Mapeia toda a memória compartilhada neste processo
+				boost::interprocess::mapped_region region(
+					object,                            // o que mapear
+					boost::interprocess::read_write    // mapeia como leitura-escrita
+				);
 
-		while (!write(result));
+				// Obtém o endereço da região mapeada
+				void* addr = region.get_address();
 
-		std::cout << "Fim" << std::endl;
-		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(10));
-	};
+				// Constrói a estrutura compartilhada na memória
+				SharedData* data = new (addr) SharedData;
 
-	std::function<void()> writer = [&]() {
-		std::cout << "Estabelecendo conexão..." << std::endl;
+				std::cout << "[waiting connection]" << std::endl;
 
-		std::string msg("readModel");
-		while (!write(msg));
+				// Lê a mensagem
+				{
+					boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(data->mutex);
+					data->condition.wait(lock);
 
-		std::cout << "Mensagem enviada: \"" << msg << "\"" << std::endl;
+					// Imprime a mensagem
+					std::cout << "[in]" << data->items << std::endl;
+					// Notifica o outro processo que o buffer está vazio
+					data->message_in = false;
+				}
 
-		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
 
-		for (msg = ""; msg.empty(); msg = read()) std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+				// Responde a mensagem
+				{
+					boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(data->mutex);
 
-		std::cout << "Fim" << std::endl;
-		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(10));
-	};
+					strcpy(data->items, "The model is being open.");
 
-	doInstanceCommunication(reader, writer);
+					std::cout << "[out]" << data->items << std::endl;
+
+					// Notifica o outro processo que há uma mensagem
+					data->condition.notify_one();
+
+					// Marca o buffer de mensagem como cheio
+					data->message_in = true;
+				}
+
+				std::cout << "[connection closed]" << std::endl;
+			}
+			catch (boost::interprocess::interprocess_exception& ex) {
+				std::cout << ex.what() << std::endl;
+			}
+		}
+		}, [&]() {
+			// Cria um objeto de memória compartilhada
+			boost::interprocess::shared_memory_object object(
+				boost::interprocess::open_only,       // somente abertura
+				shared_memory_name,                     // nome
+				boost::interprocess::read_write        // modo leitura-escrita
+			);
+
+			try {
+				// Mapeia toda a memória compartilhada neste processo
+				boost::interprocess::mapped_region region(
+					object,                                // o que mapear
+					boost::interprocess::read_write     // mapeia como leitura-escrita
+				);
+
+				// Obtém o endereço da região mapeada
+				void* addr = region.get_address();
+
+				// Obtém um ponteiro para a estrutura compartilhada
+				SharedData* data = static_cast<SharedData*>(addr);
+
+				// Escreve uma mensagem
+				{
+					boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(data->mutex);
+
+					strcpy(data->items, "read_moedel: something.egomlx");
+
+					std::cout << "[out]" << data->items << std::endl;
+
+					// Notifica o outro processo que há uma mensagem
+					data->condition.notify_one();
+
+					// Marca o buffer de mensagem como cheio
+					data->message_in = true;
+				}
+
+				// Lê a resposta.
+				{
+					boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(data->mutex);
+					data->condition.wait(lock);
+
+					// Imprime a mensagem
+					std::cout << "[in]" << data->items << std::endl;
+					// Notifica o outro processo que o buffer está vazio
+					data->message_in = false;
+				}
+			}
+			catch (boost::interprocess::interprocess_exception& ex) {
+				std::cout << ex.what() << std::endl;
+			}
+		});
+
+	// Espera algum digito antes de fechar o terminal, para verificar os resultados.
+	std::cin.ignore();
+
 	return 0;
 }
